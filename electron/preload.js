82c9ec70bd5ecfc66df3dd290ae335650d6a12d1
patela16 <anishdpatel28@gmail.com>
2025-08-12 01:43:47 -0400

    Create preload.js

---
+++ electron/preload.js
@@ -0,0 +1,18 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Menu actions
+  onMenuAction: (callback) => ipcRenderer.on('menu-action', callback),
+  removeMenuAction: (callback) => ipcRenderer.removeListener('menu-action', callback),
+  
+  // App info
+  getAppVersion: () => process.versions.electron,
+  getNodeVersion: () => process.versions.node,
+  getChromeVersion: () => process.versions.chrome,
+  
+  // Platform info
+  getPlatform: () => process.platform,
+  isDev: () => process.env.NODE_ENV === 'development'
+});
